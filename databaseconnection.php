9f827ae69e43910ba35c5d16095a6ec8ff536acc
harishaa-19 <harishaa.it19@bitsathy.ac.in>
2019-08-22 11:37:42 +0530

    first commit

--- databaseconnection.php
+++ databaseconnection.php
@@ -1,26 +1,26 @@
-
 <?php
 $servername="121.200.55.60:3307";
-$username="2019UIT1013"
-$password="2019UIT1013"
-$dbname="2019UIT1013"
+$username="2019UIT1013";
+$password="2019UIT1013";
+$dbname="2019UIT1013";
 // create connection
 $conn=new mysqli($servername,$username,$password,$dbname);
-die("connection failed":$conn->error);
+//die("connection failed":$conn->error);
 // check connection
-if ($conn->connect_error)
-{
-   die ("copnnection failed:"$conn->connect_error);
-}
-$sql="select s_no,enrollment_ no,student_name,course_name FROM student ";
+//if ($conn->connect_error)
+//{
+  // die ("copnnection failed:"$conn->connect_error);
+//}
+$sql="select * FROM student";
 $result=$conn->query($sql);
 if($result->num_rows>0){
 //output data for each row
 while($row=$result->fetch_assoc()){
 echo "s_no:".$row["s_no"]."enrollment_ no:".$row["enrollment_ no"]."course_name:".$row["course_name"]. "<br />";
+//echo "1";
 }
 }else{
-	echo "0 results";
+        echo "0 results";
 }
  $conn->close();
 ?>
