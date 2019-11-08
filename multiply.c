2a24ea827868e045f18aba13e201b6bc89e9cc90
harishaa-it19 <harishaa.it19@bitsathy.ac.in>
2019-11-08 11:19:20 +0530

    first commmit

---
+++ multiply.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        int sum=0,i,n;
+        printf("\n enter the values of n:  ");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n %d * 5 = %d",i,5*i);
+        }
+
+        getch();
+}
+
+
+
